3af4f172c8d3c4107e799eae88622040167dd7b9
avanthika <avanthika157@gmail.com>
2018-03-30 18:48:57 +0530

    New changes and react-router added

--- sourceCode/src/App.js
+++ sourceCode/src/App.js
@@ -1,21 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {
+  Switch,
+  Route
+} from 'react-router-dom';
+
+const Main = () => (<div>hksjhkdjsh</div>);
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Switch>
+        <div>
+          <Route exact path="/" component={Main} />
+        </div>
+      </Switch>
     );
   }
 }
 
-export default App;
+export default App
--- sourceCode/src/index.js
+++ sourceCode/src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import {
+  BrowserRouter as Router
+} from 'react-router-dom';
+import App from './App.js';
+import createBrowserHistory from 'history/createBrowserHistory';
+const history = createBrowserHistory();
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(
+  <Router history={history}>
+    <App />
+  </Router>,
+  document.getElementById('root')
+);
