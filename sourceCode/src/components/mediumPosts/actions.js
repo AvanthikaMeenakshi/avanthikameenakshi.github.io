330d0e16c066f1d5af5ec1e212faa204354be9c3
avanthika <avanthika157@gmail.com>
2018-03-30 21:24:41 +0530

    added medium api

--- sourceCode/src/components/mediumPosts/actions.js
+++ sourceCode/src/components/mediumPosts/actions.js
@@ -1,10 +1,24 @@
-export const LOAD_POSTS = 'LOAD_POSTS';
+import axios from 'axios';
+
+export const LOAD_POSTS_REQUEST = 'LOAD_POSTS_REQUEST';
+export const LOAD_POSTS_SUCCESS = 'LOAD_POSTS_SUCCESS';
+export const LOAD_POSTS_FAILURE = 'LOAD_POSTS_FAILURE';
 
 export const loadPosts = function () {
     return function (dispatch) {
         dispatch({
-            type: LOAD_POSTS,
+            type: LOAD_POSTS_REQUEST,
             posts: []
+        });
+        return axios.get("https://medium.com/@avanthikameenakshi/latest?format=json").then(response => response.data
+        ).then((json) => {
+            console.log(json)
+        }).catch((error) => {
+            dispatch({
+                type: LOAD_POSTS_FAILURE,
+                receivedAt: Date.now(),
+                payload: error.toString()
+            });
         });
     };
 };
--- sourceCode/src/components/mediumPosts/reducer.js
+++ sourceCode/src/components/mediumPosts/reducer.js
@@ -1,15 +1,22 @@
 import {
-    LOAD_POSTS
+    LOAD_POSTS_SUCCESS,    
+    LOAD_POSTS_REQUEST,
+    LOAD_POSTS_FAILURE
 } from './actions';
 
 const initialState = {
+    receivedAt: Date.now(),
     posts: []
 };
 
 const mediumPosts = (state = initialState, action) => {
     switch (action.type) {
-        case LOAD_POSTS:
-            return { ...state, posts: action.posts };
+        case LOAD_POSTS_REQUEST:
+            return { ...state, receivedAt: action.receivedAt };
+        case LOAD_POSTS_SUCCESS:
+            return { ...state, receivedAt: action.receivedAt, posts: action.posts };
+        case LOAD_POSTS_FAILURE:
+            return { ...state, receivedAt: action.receivedAt };
         default:
             return state;
     }
