286e28fc5da83ff400cfbc3d53c8aa4a394f7abe
avanthika <avanthika157@gmail.com>
2018-05-24 01:57:59 +0530

    source code copy

---
+++ sourceCode - Copy/src/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {
+  Router
+} from 'react-router-dom';
+import { Provider } from 'react-redux';
+import App from './App.js';
+import createBrowserHistory from 'history/createBrowserHistory';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import rootReducer from './rootReducer';
+import 'bootstrap/dist/css/bootstrap.css';
+
+const history = createBrowserHistory();
+const store = createStore(rootReducer, compose(
+  applyMiddleware(
+    thunkMiddleware
+  ),
+  window.devToolsExtension ? window.devToolsExtension() : f => f
+));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Router history={history}>
+      <App />
+    </Router>
+  </Provider>,
+  document.getElementById('root')
+);
