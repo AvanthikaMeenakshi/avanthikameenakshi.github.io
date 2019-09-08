2af81bf0bed15fb83ed95e9a3126ae908bdee3fd
Avanthika Meenakshi <avanthika157@gmail.com>
2019-09-09 00:07:50 +0100

    Changes

--- source/index.js
+++ source/index.js
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom';
 import ComingSoon from "./ComingSoon";
 import './Stylesheets/app.scss';
 
-const App = () => {
-  return <ComingSoon />
-}
+const App = () => <ComingSoon />
 
 ReactDOM.render(<App />, document.getElementById('app'))
