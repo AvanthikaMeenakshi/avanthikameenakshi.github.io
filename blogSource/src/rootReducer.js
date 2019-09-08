cd8d1546d5b19722677727b12ebc47798ca16172
Avanthika Meenakshi <avanthika157@gmail.com>
2019-09-08 23:09:15 +0100

    Deleted old source

--- blogSource/src/rootReducer.js
+++
@@ -1,8 +0,0 @@
-import mediumPosts from './components/MediumPosts/reducer'
-import { combineReducers } from 'redux';
-
-const rootReducer = combineReducers({
-    mediumPosts
-});
-
-export default rootReducer;
