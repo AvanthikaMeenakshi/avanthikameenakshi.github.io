3c910714dda9b3a3bfe80448cedcea859741cc7f
Avanthika Meenakshi <avanthika157@gmail.com>
2019-09-09 20:38:18 +0100

    Test

--- source/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import ComingSoon from "./ComingSoon";
-import './Stylesheets/app.scss';
-
-const App = () => <ComingSoon />
-
-ReactDOM.render(<App />, document.getElementById('app'))
