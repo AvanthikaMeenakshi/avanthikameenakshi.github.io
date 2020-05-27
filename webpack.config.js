082651bd9bd471bce59df2b723d699ce1e675347
Avanthika Meenakshi <avanthika157@gmail.com>
2020-05-27 14:22:20 +0100

    Removed api proxy from webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,17 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
-module.exports = (env) => ({
-  entry: "./src/index.js",
+module.exports = () => ({
+  entry: './src/index.js',
   output: {
-    filename: "[name].[hash].js",
-    path: path.resolve(__dirname, "dist"),
-    publicPath: "/",
+    filename: '[name].[hash].js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
   },
   optimization: {
     splitChunks: {
-      chunks: "all",
+      chunks: 'all',
     },
   },
   module: {
@@ -20,41 +20,35 @@ module.exports = (env) => ({
         test: /\.(js)$/,
         use: [
           {
-            loader: "babel-loader",
+            loader: 'babel-loader',
             options: {
-              presets: ["@babel/react"],
+              presets: ['@babel/react'],
             },
           },
         ],
       },
       {
         test: /\.(css|sass|scss)$/,
-        use: ["style-loader", "css-loader", "sass-loader"],
+        use: ['style-loader', 'css-loader', 'sass-loader'],
       },
       {
         test: /\.(png|jpe?g|gif)$/i,
         use: [
           {
-            loader: "file-loader",
+            loader: 'file-loader',
           },
         ],
       },
     ],
   },
-  mode: "development",
+  mode: 'development',
   devServer: {
-    historyApiFallback: true,
-    proxy: {
-      "/api": {
-        target: "http://[::1]:3000/",
-        changeOrigin: true,
-      },
-    },
+    historyApiFallback: true
   },
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      template: "./src/index.html",
+      template: './src/index.html',
     }),
   ],
 });
