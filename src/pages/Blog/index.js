05d07ab378cfa7f9216e03f83301ab0c1becdd39
Avanthika Meenakshi <avanthika157@gmail.com>
2020-08-07 22:00:43 +0100

    Few more changes

--- src/pages/Blog/index.js
+++ src/pages/Blog/index.js
@@ -1,5 +1,64 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Card, CardImg } from 'reactstrap';
+import Truncate from 'react-truncate-html';
 
-const Blog = () => <div>My blog</div>;
+const getMediumPosts = axios.get(
+  'https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@avanthikameenakshi'
+);
+
+const getLogrocketPosts = axios.get(
+  'https://api.rss2json.com/v1/api.json?rss_url=https://blog.logrocket.com/author/avanthikameenakshi/feed/'
+);
+
+const Blog = () => {
+  const [blogPosts, setBlogPosts] = useState([]);
+  useEffect(() => {
+    axios
+      .all([getMediumPosts, getLogrocketPosts])
+      .then(
+        axios.spread((...responses) => {
+          const [mediumData, logRocketData] = responses;
+          const mediumPosts = mediumData.data.items.filter((item) => {
+            if (item.categories.length > 0) {
+              item.isMedium = true;
+              return item;
+            }
+          });
+          const logRocketPosts = logRocketData.data.items;
+          setBlogPosts([...logRocketPosts, ...mediumPosts]);
+        })
+      )
+      .catch((errors) => {
+        // react on errors.
+      });
+  }, []);
+  console.log(blogPosts);
+  return (
+    <div className="repos">
+      {blogPosts.map((post) => (
+        <Card key={post.guid} className="card-resume">
+          <a
+            className="repo-link"
+            href={post.link}
+            target="_blank"
+            rel="noreferrer"
+          >
+            <span className="language">{post.language}</span>
+            <h6>{post.title}</h6>
+            {post.isMedium ? (
+              <p className="description">dkdkldjkd</p>
+            ) : (
+              <p
+                className="description"
+                dangerouslySetInnerHTML={{ __html: post.description }}
+              />
+            )}
+          </a>
+        </Card>
+      ))}
+    </div>
+  );
+};
 
 export default Blog;
