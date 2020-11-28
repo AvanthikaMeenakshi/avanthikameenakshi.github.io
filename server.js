af6cfc677a6535a5b9f9898f189d84b6051035bd
Avanthika Meenakshi <avanthika157@gmail.com>
2020-11-28 17:20:14 +0000

    Added * in server.js

--- server.js
+++ server.js
@@ -6,6 +6,6 @@ const app = express();
 
 const port = process.env.PORT || 3001;
 
-app.use('/', express.static(path.join(__dirname, 'dist')));
+app.use('*', express.static(path.join(__dirname, 'dist')));
 
 app.listen(port);
